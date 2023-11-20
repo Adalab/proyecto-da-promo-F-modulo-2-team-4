d1e7b4b83efc22df30746f96c9efa5248136749b
clarasdata <clarasdata@gmail.com>
2023-11-20 19:16:02 +0100

    sql renombrado

--- bbdd_whatflix.sql
+++ bbdd_whatflix.sql
@@ -1,3 +1,5 @@
+-- BBDD whatflix con todas las tablas
+
 CREATE DATABASE  IF NOT EXISTS `whatflix` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `whatflix`;
 -- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
